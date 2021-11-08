59a6b7da87cca100683f5453030c0afdd5a46f7f
Alessandro-NSousa <alessandro.sousa@castanhal.ufpa.br>
2021-11-08 00:11:53 -0300

    primeiro commit

---
+++ Aula.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Aula {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    for (int i = 1; i <= N; i++) {
+      int linha = i;
+      int quadrado = i * i;
+      int cubo = i * i * i;
+      System.out.printf("%d %d %d%n", linha, quadrado, cubo);
+    }
+
+    sc.close();
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# primeiros-passos
+Nesse exercício passado pelo Prof Nélio. O usuário faz uma entrada e o programa irá ter como saída o número de cada linha acomapnhada pelo quadrado e o cubo de cada linha.
